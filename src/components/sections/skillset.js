7a86e3b2334d79dd7e14da510ce0f1572209b34a
skippergoroye <oluwatosinadegoroye21@gmail.com>
2026-04-25 20:30:51 +0100

    feat: icons added to iconify

--- src/components/sections/skillset.js
+++ src/components/sections/skillset.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types'
 import Card from '../cards/Card'
-import Navbar from '../layouts/Nav'
 import { Tabs, TabsContent, TabsTrigger, TabsList } from 'components/ui/tabs'
 let Logo1 = require('../../assets/icons/logos_javascript.png')
 let Logo2 = require('../../assets/icons/git-logo.png')
@@ -26,19 +25,23 @@ const Skill = ({ mainText, onClick }) => {
 
         <div class="section4">
           <div class="skillNav textColor">
-            <Tabs defaultValue="account" className="flex flex-col">
+            <Tabs defaultValue="techskills" className="flex flex-col w-full">
 
               <TabsList>
-                <TabsTrigger value="account">
-                  Technical Skills
-                  {/* <Navbar navItem="Technical Skills" /> */}
+                <TabsTrigger value="techskills">
+                
+                  <h2 className="navItem mb-2">
+                    Technical Skills
+                  </h2>
                 </TabsTrigger>
-                <TabsTrigger value="password">
-                  Soft Skills
-                  {/* <Navbar navItem="Soft Skills" /> */}
+                <TabsTrigger value="softskills">
+              
+                  <h2 className="navItem mb-2">
+                    Soft Skills
+                  </h2>
                 </TabsTrigger>
               </TabsList>
-              <TabsContent value="account">
+              <TabsContent value="techskills">
                 <div className="flex flex-col gap-5">
                   <div className="flex justify-between items-center">
                     <Card img={Logo1} text="Javascript" />
@@ -53,11 +56,11 @@ const Skill = ({ mainText, onClick }) => {
                   <div className="flex justify-between items-center">
                     <Card img={Logo7} text="React" />
                     <Card img={Logo8} text="Vue" />
-                    <Card img={Logo8} text="And Others" />
+                    <Card img="https://img.icons8.com/color/48/nextjs.png" text="Next.js" />
                   </div>
                 </div>
               </TabsContent>
-              <TabsContent value="password">
+              <TabsContent value="softskills">
                 <div className="flex flex-col gap-5">
                   <div className="flex justify-between items-center">
                     <Card img="https://img.icons8.com/fluency/48/communication.png" text="Communication" />
@@ -70,7 +73,7 @@ const Skill = ({ mainText, onClick }) => {
                     <Card img="https://img.icons8.com/fluency/48/gears.png" text="Critical Thinking" />
                   </div>
                   <div className="flex justify-between items-center">
-                     <Card img="https://img.icons8.com/fluency/48/management.png" text="Leadership" />
+                    <Card img="https://img.icons8.com/fluency/48/management.png" text="Leadership" />
                     <Card img="https://img.icons8.com/fluency/48/paint-palette.png" text="Creativity" />
                     <Card img="https://img.icons8.com/fluency/48/briefcase.png" text="Work Ethic" />
                   </div>
